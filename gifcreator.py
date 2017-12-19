605f49f95eebf3abc95ed1d446d2c6f1a3d5bd25
Tulio Carreira <trcarreira@gmail.com>
2017-12-18 22:04:45 -0200

    Add GIF generation example - Add jpg files to /images folder - Add demo .gif file - Add original image file to main folder
    Also the image rotation was removed from script

--- gifcreator.py
+++ gifcreator.py
@@ -13,8 +13,7 @@ for filename in os.listdir("images"):
             ".tiff") or filename.endswith(".tif"):
         im = Image.open("images/{filename}".format(filename=filename))
         # decreases the image in quality in order to reduce gif's size
-        im = im.resize((im.size[0] // 2, im.size[1] // 2),
-                       Image.ANTIALIAS).rotate(-90, expand=True)
+        im = im.resize((im.size[0] // 2, im.size[1] // 2), Image.ANTIALIAS)
         im.save("images/temporary.jpg", optimize=True, quality=85)
         print("Appending file {filename}...".format(filename=filename))
         images.append(imageio.imread("images/temporary.jpg"))
